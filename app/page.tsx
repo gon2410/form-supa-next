12f7345f50f3e965be26621fbfb5098f5649e3ba
gon2410 <goonolivera@hotmail.com>
2025-07-10 09:23:05 -0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ const SaveForm = () => {
 
     useEffect(() => {
         async function getGuests() {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/`, {
                 method: "GET"
             })
 
